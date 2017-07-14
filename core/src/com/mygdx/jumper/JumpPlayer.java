dcc624dcd7a7866521e7c294969b0481f72a9099
lucaromanowski <lucaromanowski@wp.pl>
2017-07-14 18:08:53 +0200

    basic model of player
    let's begin

--- core/src/com/mygdx/jumper/JumpPlayer.java
+++ core/src/com/mygdx/jumper/JumpPlayer.java
@@ -1,7 +1,39 @@
 package com.mygdx.jumper;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class JumpPlayer extends Rectangle  {
+	
+	private Sound jumpSound;
+	private Texture texture;
+	public boolean canJump = true;
+	
+	public float jumpVelocity;
+	
+	//Constructor
+	public JumpPlayer(Texture texture) {
+		this.texture = texture;
+		this.width = texture.getWidth();
+		this.height = texture.getHeight();
+		jumpSound = Gdx.audio.newSound(Gdx.files.internal("Jump33.wav"));
+	}
+	
+	//This method is responsible for drawing our object on the screen
+	public void draw(SpriteBatch batch) {
+		batch.draw(texture, x, y);
+	}
+	
+	//This method will make Mario jump like Criss Cross!!
+	public void jump() {
+		if(canJump && jumpVelocity >= -100) {
+			jumpVelocity += 800;
+			canJump = false;
+			jumpSound.play();
+		}
+	}
 
 }
