f8a50438208b2c4e60f989ab17c794fb026c4437
lucaromanowski <lucaromanowski@wp.pl>
2017-07-14 18:41:16 +0200

    another commit

--- core/src/com/mygdx/jumper/Jumper.java
+++ core/src/com/mygdx/jumper/Jumper.java
@@ -2,13 +2,25 @@ package com.mygdx.jumper;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 
 public class Jumper extends ApplicationAdapter {
+	private Music music;
+	private Texture playerTexture, platformTexture;
+	private JumpPlayer player;
+	private Array<Platform> platformArray;
+	private OrthographicCamera camera;
+	
+	private float gravity = -20;
+	
 	SpriteBatch batch;
-	Texture img;
+	
+	
 	
 	@Override
 	public void create () {
